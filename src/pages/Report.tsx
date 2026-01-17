25a28f8ca76f43eb028d36b37bfafb5bceb9d088
Zoyaimam01 <zoyaimam444@gmail.com>
2026-01-17 16:48:34 +0530

    Integrate ReportForm into Report page

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { ReportForm } from '../components/forms/ReportForm'
 
 export default function Report() {
   return (
     <div className='min-h-screen bg-gray-50 py-12'>
       <div className='max-w-4xl mx-auto px-4'>
-        <h1 className='text-4xl font-bold mb-8'>Report an Incident</h1>
-        <p className='text-gray-600'>Report form will be added here</p>
+        <div className='mb-8'>
+          <h1 className='text-4xl font-bold mb-2'>Report an Incident</h1>
+          <p className='text-gray-600'>Help save lives by reporting accidents quickly and accurately</p>
+        </div>
+        <ReportForm />
       </div>
     </div>
   )
