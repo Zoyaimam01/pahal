44bb66b2a8716d20feedfb067bd64446e47db5ee
Zoyaimam01 <zoyaimam444@gmail.com>
2026-01-17 16:43:47 +0530

    Add Hotspots page for accident-prone areas

---
+++ src/pages/Hotspots.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hotspots() {
+  return (
+    <div className='min-h-screen bg-gray-50 py-12'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <h1 className='text-4xl font-bold mb-8'>Accident Hotspots</h1>
+        <div className='bg-white rounded-lg shadow p-6'>
+          <p className='text-gray-600'>Interactive map will be displayed here</p>
+        </div>
+      </div>
+    </div>
+  )
+}
