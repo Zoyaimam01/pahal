492d43c1a73d123831a45716a4df34056eb5e13d
Zoyaimam01 <zoyaimam444@gmail.com>
2026-01-17 16:43:31 +0530

    Create Report page skeleton

---
+++ src/pages/Report.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Report() {
+  return (
+    <div className='min-h-screen bg-gray-50 py-12'>
+      <div className='max-w-4xl mx-auto px-4'>
+        <h1 className='text-4xl font-bold mb-8'>Report an Incident</h1>
+        <p className='text-gray-600'>Report form will be added here</p>
+      </div>
+    </div>
+  )
+}
