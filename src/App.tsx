f4f1e0f4320ebba792b382ea4384ca22213ff2c7
Zoyaimam01 <zoyaimam444@gmail.com>
2026-01-17 16:47:10 +0530

    Configure routing for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import PublicLayout from './components/PublicLayout'
+import AdminLayout from './components/AdminLayout'
 import Home from './pages/Home'
+import Report from './pages/Report'
+import Hotspots from './pages/Hotspots'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
+import Analytics from './pages/Analytics'
+import SmartCamera from './pages/SmartCamera'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<PublicLayout />}>
+          <Route index element={<Home />} />
+          <Route path='report' element={<Report />} />
+          <Route path='hotspots' element={<Hotspots />} />
+        </Route>
+
+        <Route path='/admin' element={<AdminLogin />} />
+
+        <Route path='/admin' element={<AdminLayout />}>
+          <Route path='dashboard' element={<AdminDashboard />} />
+          <Route path='reports' element={<Report />} />
+          <Route path='smart-camera' element={<SmartCamera />} />
+          <Route path='analytics' element={<Analytics />} />
+        </Route>
+
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
